4c4fa586a51ab747532748e60c145824e4b4238e
laveberry <laveberry7@gmail.com>
2023-03-27 13:36:53 +0900

    상품 수정

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -87,6 +87,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    //상품수정 폼 보기
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    //상품 수정 처리
+    @PostMapping ("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        //redirect사용법
+        return "redirect:/basic/items/{itemId}";
+    }
+
     /*
     테스트용 데이터 추가
     */
